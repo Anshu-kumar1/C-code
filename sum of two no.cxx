8e5fca76a5294ff17a7b61dde5a3ae301e2e899a
Anshu-kumar1 <138924132+Anshu-kumar1@users.noreply.github.com>
2023-08-17 09:32:00 +0530

    Add files via upload

---
+++ sum of two no.cxx
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,sum=0;
+    cout<<"enter 1st no. -"<<endl;
+    cin>>a;
+    cout<<"enter 2nd no. - "<<endl;
+    cin>>b;
+    sum=a+b;
+    cout<<"sum of two no. is  "<<sum<<endl;
+     
+     return 0;
+     
+}
