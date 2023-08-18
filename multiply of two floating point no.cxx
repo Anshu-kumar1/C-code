30d4a20cce2583896088c95494571c1d0eaa4b2f
Anshu-kumar1 <138924132+Anshu-kumar1@users.noreply.github.com>
2023-08-19 05:11:44 +0530

    Add files via upload

---
+++ multiply of two floating point no.cxx
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    float num1,num2,result;
+    printf("enter 1st no.");
+    scanf("%f",&num1);
+     printf("enter 2nd no.");
+     scanf("%f",&num2);
+    result=num1*num2;
+     printf("multiply of two floating-point no. is %f",result);
+    return 0;
+}
