82a9a6bad31eab9b836b7cfe93baf3fd1edc2728
Anshu-kumar1 <138924132+Anshu-kumar1@users.noreply.github.com>
2023-08-19 06:03:17 +0530

    Add files via upload

---
+++ check even or odd no..cxx
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"enter a number:"<<endl;
+    cin>>num;
+    if(num%2==0)
+    cout<<"even"<<endl;
+    else{
+    cout<<"odd"<<endl;
+    }
+    return 0;
+}
